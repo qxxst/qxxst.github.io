ef8953c32999ae34624fbd0dc2d354e1ecc56a19
qxxst <31394614+qxxst@users.noreply.github.com>
2024-02-29 21:22:47 -0600

    Remove unused javascript files

--- docs/lib/qm.js
+++
@@ -1,7 +0,0 @@
-// qm.js by qxxst
-function process() {
-    var images = document.querySelectorAll('img:not([alt])');
-    for (var i = 0; i < images.length; i++) {
-        images[i].style.border = '5px solid red';
-    }
-}
