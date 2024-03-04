cc80d06710eee61a5e64d5ddc33d9510657b0991
qxxst <31394614+qxxst@users.noreply.github.com>
2024-03-03 18:16:49 -0600

    Add support for multiple backgrounds

--- docs/site.js
+++ docs/site.js
@@ -2,8 +2,16 @@
 document.addEventListener('DOMContentLoaded', function () {
     const modal = document.getElementById('agreementModal');
     const agreeButton = document.getElementById('agreeButton');
+    const content = document.getElementById('content');
+    const preAgreeBackground = "qm-breathing";
+    const postAgreeBackground = "qm-breathing";
+    content.classList.add(preAgreeBackground);
     modal.style.display = 'block';
     agreeButton.addEventListener('click', function () {
+        if (preAgreeBackground !== postAgreeBackground) {
+            content.classList.remove(preAgreeBackground);
+            content.classList.add(postAgreeBackground);
+        }
         modal.style.display = 'none';
         const audioUrl = "https://cdn.discordapp.com/attachments/1213958929894346823/1213971035209212005/rb-2024-low-pass.wav?ex=65f769a1&is=65e4f4a1&hm=17e577a6e92742bd97f62eb41a9458ae6c8e6624973c255b683c792f577c0cda&";
         const audio = new Audio(audioUrl);
--- src/site.ts
+++ src/site.ts
@@ -1,11 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
     const modal = document.getElementById('agreementModal') as HTMLElement;
     const agreeButton = document.getElementById('agreeButton') as HTMLElement;
+    const content = document.getElementById('content') as HTMLElement;
+
+    const preAgreeBackground: string = "qm-breathing";
+    const postAgreeBackground: string = "qm-breathing";
+
+    content.classList.add(preAgreeBackground);
 
     modal.style.display = 'block';
 
     agreeButton.addEventListener('click', function() {
+        if (preAgreeBackground !== postAgreeBackground) {
+            content.classList.remove(preAgreeBackground);
+            content.classList.add(postAgreeBackground);
+        }
+
         modal.style.display = 'none';
+
         const audioUrl: string = "https://cdn.discordapp.com/attachments/1213958929894346823/1213971035209212005/rb-2024-low-pass.wav?ex=65f769a1&is=65e4f4a1&hm=17e577a6e92742bd97f62eb41a9458ae6c8e6624973c255b683c792f577c0cda&";
         const audio = new Audio(audioUrl);
         audio.volume = 0.1;
