2ecb2267f02ddb568536b92cd474c9a374e5f938
qxxst <qxxst@proton.me>
2024-03-05 02:20:23 -0600

    add cookie that (probably) works :)

--- docs/site.js
+++ docs/site.js
@@ -1,13 +1,37 @@
 "use strict";
 document.addEventListener('DOMContentLoaded', function () {
+    function setCookie(name, value, days) {
+        const date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        const expires = "expires=" + date.toUTCString();
+        document.cookie = name + "=" + value + ";" + expires + ";path=/";
+    }
+    function getCookie(name) {
+        const cookieName = name + "=";
+        const decodedCookie = decodeURIComponent(document.cookie);
+        const cookieArray = decodedCookie.split(';');
+        for (let i = 0; i < cookieArray.length; i++) {
+            let cookie = cookieArray[i];
+            while (cookie.charAt(0) === ' ') {
+                cookie = cookie.substring(1);
+            }
+            if (cookie.indexOf(cookieName) === 0) {
+                return cookie.substring(cookieName.length, cookie.length);
+            }
+        }
+        return "";
+    }
     const modal = document.getElementById('agreementModal');
     const agreeButton = document.getElementById('agreeButton');
     const content = document.getElementById('content');
     const preAgreeBackground = "qm-breathing";
     const postAgreeBackground = "qm-breathing";
     content.classList.add(preAgreeBackground);
-    modal.style.display = 'block';
-    agreeButton.addEventListener('click', function () {
+    const agreedToTerms = getCookie("agreedToTerms");
+    if (agreedToTerms == false || agreedToTerms == undefined || agreedToTerms == null) {
+        modal.style.display = 'block';
+    }
+    function removeModal() {
         if (preAgreeBackground !== postAgreeBackground) {
             content.classList.remove(preAgreeBackground);
             content.classList.add(postAgreeBackground);
@@ -23,5 +47,9 @@ document.addEventListener('DOMContentLoaded', function () {
                 this.play();
             }, false);
         }, 500);
+    }
+    agreeButton.addEventListener('click', function () {
+        setCookie("agreedToTerms", true, 365);
+        removeModal();
     });
 });
--- src/site.ts
+++ src/site.ts
@@ -1,4 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
+    function setCookie(name: string, value: any, days: number) {
+        const date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+
+        const expires = "expires=" + date.toUTCString();
+        document.cookie = name + "=" + value + ";" + expires + ";path=/";
+    }
+
+    function getCookie(name: string) {
+        const cookieName = name + "=";
+        const decodedCookie = decodeURIComponent(document.cookie);
+        const cookieArray = decodedCookie.split(';');
+        for (let i = 0; i < cookieArray.length; i++) {
+            let cookie = cookieArray[i];
+            while (cookie.charAt(0) === ' ') {
+                cookie = cookie.substring(1);
+            }
+            if (cookie.indexOf(cookieName) === 0) {
+                return cookie.substring(cookieName.length, cookie.length);
+            }
+        }
+        return "";
+    }
+
     const modal = document.getElementById('agreementModal') as HTMLElement;
     const agreeButton = document.getElementById('agreeButton') as HTMLElement;
     const content = document.getElementById('content') as HTMLElement;
@@ -8,9 +32,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
     content.classList.add(preAgreeBackground);
 
-    modal.style.display = 'block';
+    const agreedToTerms: boolean = getCookie("agreedToTerms") as unknown as boolean;
+    if (agreedToTerms == false || agreedToTerms == undefined || agreedToTerms == null) {
+        modal.style.display = 'block';
+    }
 
-    agreeButton.addEventListener('click', function() {
+    function removeModal() {
         if (preAgreeBackground !== postAgreeBackground) {
             content.classList.remove(preAgreeBackground);
             content.classList.add(postAgreeBackground);
@@ -28,5 +55,10 @@ document.addEventListener('DOMContentLoaded', function() {
                 this.play();
             }, false);
         }, 500);
+    }
+
+    agreeButton.addEventListener('click', function() {
+        setCookie("agreedToTerms", true, 365);
+        removeModal();
     });
 });
